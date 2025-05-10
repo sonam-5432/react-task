8084e16f14cfb986e21076d6cf66a4ca2af43daf
sonam-5432 <sonamsingariya@gmail.com>
2025-05-10 16:02:19 +0530

    Change in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/react-task/',  // 👈 Add this line
   plugins: [react()],
 })
